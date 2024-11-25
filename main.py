0dba18bfef61012a7b747899acc21324c8294a0f
greek soon <greek_soon@hotmail.com>
2024-11-25 18:30:43 +0800

    init

---
+++ main.py
@@ -0,0 +1,21 @@
+from jinja2 import Environment, PackageLoader, select_autoescape
+env = Environment(
+    loader=PackageLoader("resume"),
+    autoescape=select_autoescape()
+)
+
+template = env.get_template("index.html.jinja")
+
+import json
+import pkgutil
+
+content = pkgutil.get_data('resume', 'resum.json')
+if content is None:
+  print('can not find resume.json in app dir')
+  exit(-1)
+
+resume = json.loads(content.decode('utf-8'))
+# print(resume['basics'])
+
+with open('index.html', 'w+') as f:
+  f.write(template.render(resume=resume))
