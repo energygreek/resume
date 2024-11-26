a11e2ec5a7ae1b4d6878218ba6498535fbd34878
greek soon <greek_soon@hotmail.com>
2024-11-26 18:13:24 +0800

    support i18n

--- README.md
+++ README.md
@@ -3,4 +3,21 @@
 pip install jinja
 ## Usage
 * Update resume json file in resume dir
-* python main.py
+* python main.py gen
+
+## 多语言支持
+默认是英文简历，其他语言也可以类似添加。先安装依赖如"sudo apt install gettext -y"
+生成中文简历: python main.py gen -l zh_CN
+
+## 更新简历模板
+如果对模板文件的翻译部分更改，需要同时更新翻译文件
+
+### 更新英文
+1. 更新po文件， python main.py collectmsg -l en_US
+2. 补充翻译目标内容
+3. 生成mo, msgfmt lang/en_US/LC_MESSAGES/resume.po -o lang/en_US/LC_MESSAGES/resume.mo
+
+### 更新中文
+1. 更新po文件， python main.py collectmsg -l zh_CN
+2. 补充翻译目标内容
+3. 生成mo, msgfmt lang/zh_CN/LC_MESSAGES/resume.po -o lang/zh_CN/LC_MESSAGES/resume.mo
--- main.py
+++ main.py
@@ -1,21 +1,89 @@
+from os import curdir
 from jinja2 import Environment, PackageLoader, select_autoescape
-env = Environment(
-    loader=PackageLoader("resume"),
-    autoescape=select_autoescape()
-)
-
-template = env.get_template("index.html.jinja")
-
 import json
 import pkgutil
+import click
+import gettext
 
-content = pkgutil.get_data('resume', 'resum.json')
-if content is None:
-  print('can not find resume.json in app dir')
-  exit(-1)
+env = Environment(
+    loader=PackageLoader("resume"),
+    autoescape=select_autoescape(),
+    extensions=["jinja2.ext.i18n"],
+)
 
-resume = json.loads(content.decode('utf-8'))
-# print(resume['basics'])
+def main(lang:str):
+    template = env.get_template("index.html.jinja")
 
-with open('index.html', 'w+') as f:
-  f.write(template.render(resume=resume, basics=resume['basics']))
+    localedir = curdir + '/lang'
+    env.install_gettext_translations(translations=gettext.translation(
+        domain='resume', localedir=localedir, languages=[lang]))
+
+    content = pkgutil.get_data("resume", "resum.json")
+    if content is None:
+        print("can not find resume.json in app dir")
+        exit(-1)
+
+    resume = json.loads(content.decode("utf-8"))
+    # print(resume['basics'])
+
+    with open("index.html", "w+") as f:
+        f.write(template.render(resume=resume, basics=resume["basics"]))
+
+
+@click.group()
+def cli():
+    """A simple command line tool."""
+    pass
+
+
+@cli.command()
+def testtrans():
+    """test translate text"""
+    localedir = curdir + '/lang'
+    gettext.bindtextdomain('resume', localedir)
+    gettext.textdomain('resume')
+    print(gettext.gettext("Links"))
+
+
+@cli.command()
+@click.option('-l', '--lang', help="language, en_US(defualt) or zh_CN", type=str, default='en_US')
+def collectmsg(lang:str):
+    """generate po file"""
+    template = env.get_template("index.html.jinja")
+    po_file = curdir + f'/lang/{lang}/LC_MESSAGES/resume.po'
+    with open(po_file, 'w+') as po:
+        po.write(
+            'msgid ""\n'
+            'msgstr ""\n'
+            '"Content-Type: text/plain; charset=UTF-8\\n"\n'
+            '"Project-Id-Version: MetaSearch 0.1-dev\\n"\n'
+            '"MIME-Version: 1.0\\n"\n'
+        )
+
+        msg_strings: list[str] = []
+        if template.filename is None:
+            print("template file is not find")
+            return
+
+        with open(template.filename) as f:
+            for (lineno, function, message) in env.extract_translations(f.read()):
+                if message not in msg_strings:
+                    msg_strings.append(message)
+                    po.write(f"#{function}:{lineno}\n")
+                    po.write(f"msgid \"{message}\"\n")
+                    po.write('msgstr ""\n\n')
+
+
+@cli.command()
+@click.option('-l', '--lang', help="language, en_US(defualt) or zh_CN", type=str, default='en_US')
+def gen(lang:str):
+    """generate html"""
+    main(lang)
+
+
+cli.add_command(testtrans)
+cli.add_command(collectmsg)
+cli.add_command(gen)
+
+if __name__ == "__main__":
+    cli()
